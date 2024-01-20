ff365bfbd07081eae670ca017dec399c3e8f0c8e
Ved <vedp7900@gmail.com>
2024-01-20 16:50:56 -0500

    fixed issue

--- app.py
+++ app.py
@@ -4,31 +4,33 @@ import serial
 # Initializes the Flask application
 app = Flask(__name__)
 
+ser = serial.Serial('COM5', 9600)
 
-# # Route file for index.html page
-@app.route('/', methods=["GET", "POST"])
+# Route file for index.html page
+@app.route('/')
 def main_page():
-    return render_template("index.html")
+    data = ser.readline().decode('utf-8').strip()
+    # Process the data (you may want to parse it and perform specific actions)
+    if data.startswith("Soil Moisture:"):
+        global sensor_value
+        sensor_value = data.split(":")[1]
+        print(f"Received sensor data: {sensor_value}")
+    return render_template("index.html", sensor_value = sensor_value)
 
 
 
 # Get data from the Arduino and move it to the server
-ser = serial.Serial('COM5', 9600)
 
-while True:
-    try:
-        # Read data from Serial
-        data = ser.readline().decode('utf-8').strip()
+# while True:
+#     try:
+#         # Read data from Serial
+#         data = ser.readline().decode('utf-8').strip()
 
-        # Process the data (you may want to parse it and perform specific actions)
-        if data.startswith("Soil Moisture:"):
-            sensor_value = data.split(":")[1]
-            print(f"Received sensor data: {sensor_value}")
-            # Route file for index.html page
-            @app.route('/', methods=["GET", "POST"])
-            def main_page():
-                return render_template("index.html", sensor_value = sensor_value)
-            
-    except KeyboardInterrupt:
-        # Close the serial port when the script is interrupted (e.g., Ctrl+C)
-        ser.close()
+#         # Process the data (you may want to parse it and perform specific actions)
+#         if data.startswith("Soil Moisture:"):
+#             sensor_value = data.split(":")[1]
+#             print(f"Received sensor data: {sensor_value}")
+
+#     except KeyboardInterrupt:
+#         # Close the serial port when the script is interrupted (e.g., Ctrl+C)
+#         ser.close()
