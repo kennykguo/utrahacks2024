017561f07ad082e6d973533df3f041bb4a2f1ca2
Kenny Guo <kennykguo@gmail.com>
2024-01-20 22:10:09 -0500

    routing html pages

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template
 import serial
 
 # Initializes the Flask application
 app = Flask(__name__)
 
-ser = serial.Serial('COM5', 9600)
+# ser = serial.Serial('COM5', 9600)
 
 # Route file for index.html page
+# When the homepage is accessed
 @app.route('/')
-def main_page():
+def index():
     # Get data from the Arduino
-    data = ser.readline().decode('utf-8').strip()
+    # data = ser.readline().decode('utf-8').strip()
     # Get the soil moisture data
-    if data.startswith("Soil Moisture:"):
-        global sensor_value
-        sensor_value = data.split(":")[1]
-        print(f"Received sensor data: {sensor_value}")
-    return render_template("index.html", sensor_value = sensor_value)
+    # if data.startswith("Soil Moisture:"):
+        # global sensor_value
+        # sensor_value = data.split(":")[1]
+        # print(f"Received sensor data: {sensor_value}")
+    return render_template("index.html")
+# Make sure to pass in a value after
 
 
+@app.route('/aboutus')
+def index():
+    return render_template("aboutus.html")
+
+
+@app.route('/leaderboard')
+def index():
+    return render_template("leaderboard.html")
+
+@app.route('/dataanalytics')
+def index():
+    return render_template("dataanalytics.html")
 
 # Get data from the Arduino and move it to the server
 
