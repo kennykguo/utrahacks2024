aaf2379b24f176bb790626564a965095c5c21122
Kenny Guo <kennykguo@gmail.com>
2024-01-20 21:22:07 -0500

    updated commments

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ ser = serial.Serial('COM5', 9600)
 # Route file for index.html page
 @app.route('/')
 def main_page():
+    # Get data from the Arduino
     data = ser.readline().decode('utf-8').strip()
-    # Process the data (you may want to parse it and perform specific actions)
+    # Get the soil moisture data
     if data.startswith("Soil Moisture:"):
         global sensor_value
         sensor_value = data.split(":")[1]
